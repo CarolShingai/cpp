e1fccc3abc7680a753c6783ca772d3194437b7fc
CarolShingai <carolineshingai@gmail.com>
2025-04-17 20:46:05 -0300

    ex00 testes prontos

--- cpp01/ex00/include/Zombie.hpp
+++ cpp01/ex00/include/Zombie.hpp
@@ -4,6 +4,16 @@
 # include <string>
 # include <cstdlib>
 
+
+// Colors
+# define GREEN	"\033[92m"
+# define RED	"\033[91m"
+# define CYAN	"\033[96m"
+# define YELLOW	"\033[93m"
+# define BLUE	"\033[94m"
+# define MAGENT	"\033[95m"
+# define RESET	"\033[0m"
+
 class Zombie
 {
 	private:
--- cpp01/ex00/src/main.cpp
+++ cpp01/ex00/src/main.cpp
@@ -2,11 +2,29 @@
 
 int main(void)
 {
-	std::cout << "Creating a new Zombie in the Heap" << std::endl;
+	const std::string z_name[5] = {"Bob", "Alice", "Chad", "Diana", "Eve"};
+
+	std::cout << GREEN << "===== Início dos testes =====\n" << RESET << std::endl;
+	std::cout << YELLOW << "First Teste"<< RESET << std::endl;
+	std::cout << YELLOW <<"Creating a new Zombie in the Heap" << RESET << std::endl;
+	std::cout << "Deleting the new Zombie..." << std::endl;
 	Zombie* zombie = newZombie("Bob");
 	zombie->announce();
-	std::cout << "Creating a Zombie with randomChump in the Stack" << std::endl;
-	randomChump("Chump");
-	std::cout << "Deleting the new Zombie..." << std::endl;
 	delete zombie;
+	if (!zombie)
+		std::cerr << RED << "Zombie still on the heap" << RESET << std::endl;
+	std::cout<< YELLOW << "Second Teste" << RESET << std::endl;
+	std::cout << YELLOW <<"Creating a multiple Zombies in the Heap" << RESET << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		Zombie *zombie = newZombie(z_name[i]);
+		zombie->announce();
+		delete zombie;
+		if (!zombie)
+			std::cerr << RED << "Zombie still on the heap" << RESET << std::endl;
+	}
+	std::cout<< YELLOW << "Third Teste" << RESET << std::endl;
+	std::cout << YELLOW << "Creating a Zombie with randomChump in the Stack" << RESET << std::endl;
+	randomChump("Chump");
+	std::cout << GREEN << "===== Fim dos testes =====" << RESET <<std::endl;
 }
