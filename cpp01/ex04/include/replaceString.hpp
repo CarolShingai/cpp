cfcf7f9cb75af6e5f008bad9d1b3cd755ef43d0a
CarolShingai <carolineshingai@gmail.com>
2025-04-20 15:00:31 +0000

    ex04 pronto

--- cpp01/ex04/include/replaceString.hpp
+++ cpp01/ex04/include/replaceString.hpp
@@ -4,6 +4,7 @@
 # include <string>
 # include <iostream>
 # include <fstream>
+
 	// Colors
 # define GREEN	"\033[92m"
 # define RED	"\033[91m"
@@ -14,16 +15,53 @@
 
 namespace replaceFunctions
 {
-	int readFile(const std::string &filename)
+	int errorMessage(const std::string &message)
 	{
+		std::cerr << RED << message << RESET << std::endl;
+		return (0);
+	}
+
+	void replaceString(std::string &line, const std::string &str1, const std::string &str2, int &count)
+	{
+		std::string replacedLine;
+		size_t pos = 0;
+		size_t found = 0;
+		while ((found = line.find(str1, pos)) != std::string::npos)
+		{
+			replacedLine.append(line, pos, found - pos);
+			replacedLine += str2;
+			pos = found + str1.length();
+			count++;
+		}
+		replacedLine.append(line, pos, line.length() - pos);
+		line = replacedLine;
+	}
+
+	int readFile(const std::string &filename, const std::string &str1, const std::string &str2)
+	{
+		int count = 0;
 		std::ifstream file(filename.c_str());
 		if (!file)
+			return(errorMessage("Error opening the file."));
+		if (file.peek() == EOF)
+			return (errorMessage("Error: file is empty."));
+		std::cout << GREEN << "File opening with success" << RESET << std::endl;
+		std::ofstream outputInFile((filename + ".replace").c_str());
+		if (!outputInFile)
+			return (errorMessage("Error opening the file."));
+		if (str1.empty())
+			return (errorMessage("Error: str1 is empty."));
+		std::string line;
+		while (std::getline(file, line))
 		{
-			std::cerr << RED << "Error opening the file." << RESET << std::endl;
-			return (0);
+			replaceString(line, str1, str2, count);
+			outputInFile << line << std::endl;
 		}
-		std::cout << GREEN << "File opening with success";
-		// std::ofstream outputInFile(filename);
+		if (count == 0)
+			return (errorMessage("Error: str1 not found in the file."));
+		std::cout << GREEN << "String replaced with success" << RESET << std::endl;
+		file.close();
+		outputInFile.close();
 		return (1);
 	}
 }
--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -1,6 +1,22 @@
 #include "../include/replaceString.hpp"
 
+
+
+
 int main()
 {
-	replaceFunctions::readFile("main.cpp");
+    std::cout << GREEN << "===== Início dos testes =====\n" << RESET << std::endl;
+    std::cout << YELLOW << "test 1: Non-Existent File" << RESET << std::endl;
+    replaceFunctions::readFile("nonexistent.txt", "x", "y");
+    std::cout << YELLOW << "test 2: Empty File" << RESET << std::endl;
+    replaceFunctions::readFile("emptyfile.txt", "x", "y");
+    std::cout << YELLOW << "test 3: Empty str1" << RESET << std::endl;
+    replaceFunctions::readFile("/home/carol/42sp/cpp00/cpp01/ex04/src/main.cpp", "", "x");
+    std::cout << YELLOW << "test 4: Str1 Not Found" << RESET << std::endl;
+    replaceFunctions::readFile("text.txt", "batata", "Cadete");
+    std::cout << YELLOW << "test 5: Right File" << RESET << std::endl;
+    replaceFunctions::readFile("text.txt", "Processador", "Cadete");
+    std::cout << YELLOW << "test 6: Correct Path" << RESET << std::endl;
+	replaceFunctions::readFile("/home/carol/42sp/cpp00/cpp01/ex04/src/main.cpp", "main", "bacon");
+
 }
