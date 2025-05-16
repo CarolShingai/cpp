d45d1245a0b2fce2f54fa154a20da94e6347e6fd
CarolShingai <carolineshingai@gmail.com>
2025-05-15 21:46:36 -0300

    erros

--- cpp05/ex01/src/form.cpp
+++ cpp05/ex01/src/form.cpp
@@ -10,14 +10,18 @@ Form::Form(const std::string name, const int gradeSign, const int gradeExecute) 
 }
 
 Form::Form(const Form &copy){
-	this = *copy;
+	_name = copy._name;
+	_isSigned = copy._isSigned;
+	_gradeSign = copy._gradeSign;
+	_gradeExecute = copy._gradeExecute;
 }
 
 Form &Form::operator=(const Form &copy){
-	if (this != &copy)
+	if (this != &copy){
 		this->_name = copy._name;
 		this->_isSigned = copy._isSigned;
 		this->_gradeSign = copy._gradeSign;
 		this->_gradeExecute = copy._gradeExecute;
-
+	}
+	return *this;
 }
