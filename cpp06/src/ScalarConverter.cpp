4808457dcd9deb85e92a6caa9649f41ea82953d4
CarolShingai <carolineshingai@gmail.com>
2025-06-27 16:23:13 -0300

    metodos base implementados

---
+++ cpp06/src/ScalarConverter.cpp
@@ -0,0 +1,13 @@
+#include "../include/ScalarConverter.Hpp"
+
+ScalarConverter::ScalarConverter(){}
+
+ScalarConverter::~ScalarConverter(){}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &copy){}
+
+ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &other){}
+
+void	ScalarConverter::convert(std::string &literal){
+	
+}
