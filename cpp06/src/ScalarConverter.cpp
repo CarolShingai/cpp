0ea98e372886fa3cbb12e9aa8a97865aa8c67012
CarolShingai <carolineshingai@gmail.com>
2025-07-04 20:48:06 -0300

    finalmente andando

--- cpp06/src/ScalarConverter.cpp
+++ cpp06/src/ScalarConverter.cpp
@@ -9,88 +9,51 @@ ScalarConverter::ScalarConverter(const ScalarConverter &copy){}
 ScalarConverter	&ScalarConverter::operator=(const ScalarConverter &other){}
 
 // identificar o tipo
-e_type identifyType(std::string &data){
-	if (isPseudoLiteral(data))
-		return PSEUDOLITERAL;
-	else if (isAlpha(data))
-		return CHAR;
-	e_type num = isNumericType(data);
-	return num;
-}
 
-bool	isPseudoLiteral(std::string &data){
-	return data == "-inff" || data == "+inff" || data == "nanf" ||
-	data == "-inf" || data == "+inf" || data == "nan";
-}
-
-bool	isAlpha(std::string &data){
-	for (int i = 0; i < data.length(); i++){
-		if (!std::isalpha(data[i]))
-		return false;
-	}
-	return true;
-}
-
-// verificar se consegue transformar par INT, FLOAT, DOBLE
-e_type isNumericType(std::string &data){
-	if (isInt(data))
-		return INT;
-	else if (isFloat(data))
-		return FLOAT;
-	else if (isDouble(data))
-		return DOUBLE;
-	else
-		return OTHER;
-}
-
-bool isInt(std::string &data){
-	try{
-		/* stoi transforma data para int e pega a posição do ultimo
-		 caracter casteado. Ex: 255abc -> no número terá casting*/
-		size_t pos;
-
-		std::stoi(data, &pos);
-		return pos == data.size();
-		// (...)pega qualquer exceção
-	} catch(...){
-		return false;
-	}
-}
-
-bool isFloat(std::string &data){
-	// back retorna o ultimo caracter de data
-	// stof faz a transfomação para float
-	try{
-		if (data.back() == 'f')
-			std::stof(data);
-		else
-			return false;
-		return true;
-	} catch(...){
-		return false;
-	}
-}
-
-bool isDouble(std::string &data){
-	try{
-		size_t pos;
-		std::stod(data, &pos);
-		return pos == data.size();
-	} catch(...){
-		return false;
-	}
-}
 
 void	ScalarConverter::convert(std::string &data){
 	e_type type = identifyType(data);
-	int n = 0;
-	if (data.empty())
-		n = 0;
-	for (int i = 0; i < data.size(); i++)
-		if (!std::isprint(data[i]))
-			
 
+	char c;
+	int i;
+	float f;
+	double d;
+	bool charImpossible = false;
+	bool intImpossible = false;
 
-
-
+	switch (type)
+	{
+		case CHAR:
+			if (data.size() == 1){
+				c = data[0];
+				i = static_cast<int>(c);
+				f = static_cast<float>(i);
+				d = static_cast<float>(i);
+			}
+			break;
+		case INT:
+			i = std::stoi(data);
+			c = static_cast<char>(i);
+			f = static_cast<float>(i);
+			d = static_cast<double>(i);
+			break;
+		case FLOAT:
+			f = std::stof(data);
+			i = static_cast<int>(f);
+			c = static_cast<char>(i);
+			d = static_cast<double>(i);
+			break;
+		case DOUBLE:
+			d = std::stod(data);
+			i = static_cast<int>(d);
+			c = static_cast<char>(i);
+			f = static_cast<float>(i);
+			break;
+		case PSEUDOLITERAL:
+			charImpossible = true;
+			intImpossible = true;
+			f = std::stof(data);
+			d = std::stod(data);
+			break;
+	}
 }
--- cpp06/src/identifyType.cpp
+++ cpp06/src/identifyType.cpp
@@ -1,4 +1,81 @@
 #include "../include/ScalarConverter.Hpp"
 
+e_type identifyType(std::string &data){
+	if (isPseudoLiteral(data))
+		return PSEUDOLITERAL;
+	else if (isAlpha(data))
+		return CHAR;
+	e_type num = isNumericType(data);
+	return num;
+}
 
+bool	isPseudoLiteral(std::string &data){
+	return data == "-inff" || data == "+inff" || data == "nanf" ||
+	data == "-inf" || data == "+inf" || data == "nan";
+}
 
+bool	isAlpha(std::string &data){
+	for (int i = 0; i < data.length(); i++){
+		if (!std::isalpha(data[i]))
+		return false;
+	}
+	return true;
+}
+
+// verificar se consegue transformar par INT, FLOAT, DOBLE
+e_type isNumericType(std::string &data){
+	if (isInt(data))
+		return INT;
+	else if (isFloat(data))
+		return FLOAT;
+	else if (isDouble(data))
+		return DOUBLE;
+	else
+		return OTHER;
+}
+
+bool isInt(std::string &data){
+	try{
+		/* stoi transforma data para int e pega a posição do ultimo
+		 caracter casteado. Ex: 255abc -> no número terá casting*/
+		size_t pos;
+
+		std::stoi(data, &pos);
+		return pos == data.size();
+		// (...)pega qualquer exceção
+	} catch(...){
+		return false;
+	}
+}
+
+bool isFloat(std::string &data){
+	// back retorna o ultimo caracter de data
+	// stof faz a transfomação para float
+	try{
+		if (data.back() == 'f')
+			std::stof(data);
+		else
+			return false;
+		return true;
+	} catch(...){
+		return false;
+	}
+}
+
+bool isDouble(std::string &data){
+	try{
+		size_t pos;
+		std::stod(data, &pos);
+		return pos == data.size();
+	} catch(...){
+		return false;
+	}
+}
+
+bool isUnprinteble(std::string &data){
+	for (int i = 0; i < data.size(); i++){
+		if (!isprint(data[i]))
+			return true;
+	}
+	return false;
+}
