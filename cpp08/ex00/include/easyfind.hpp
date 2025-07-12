af9d079aa7af77278f0e02da2a7ee0277d33b1ba
CarolShingai <carolineshingai@gmail.com>
2025-07-11 21:21:02 -0300

    inicio cpp08

---
+++ cpp08/ex00/include/easyfind.hpp
@@ -0,0 +1,15 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+#include <algorithm>
+#include <list>
+
+template <typename T>
+class EasyFind {
+	private:
+		std::list<T> _container;
+	public:
+		
+
+};
+
+#endif
