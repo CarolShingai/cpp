04cdfe6861936da72760e403c525d5877341204b
CarolShingai <carolineshingai@gmail.com>
2025-07-19 04:38:24 +0000

    ex01 completo

--- cpp08/ex01/include/Span.hpp
+++ cpp08/ex01/include/Span.hpp
@@ -4,6 +4,16 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <limits>
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define PURPLE "\033[38;5;93m"
+# define CYAN "\033[36m"
+# define ORANGE "\033[38;5;208m"
+# define RESET "\033[0m"
 
 class Span {
 	private:
@@ -17,6 +27,7 @@ class Span {
         Span &operator=(const Span &other);
 
         void addNumber(int new_nbr);
+        void addMultipleNumbers(std::vector<int>::iterator begin, std::vector<int>::iterator end);
         int shortestSpan(void);
         int longestSpan(void);
 };
--- cpp08/ex01/src/Span.cpp
+++ cpp08/ex01/src/Span.cpp
@@ -10,7 +10,6 @@ Span::Span(unsigned int n) : _n(n){
 
 Span::~Span(){}
 
-// talvez fazer uma verificacao se n = 0;
 Span::Span(const Span &other) : _n(other._n){
     this->_n = other._n;
     this->_container = other._container;
@@ -25,24 +24,42 @@ Span &Span::operator=(const Span &other){
 }
 
 void Span::addNumber(int new_n){
-    if (this->_container.size() < this->_n) 
-        throw std::length_error("Span limit reached: cannot add more numbers");
+    if (this->_container.size() >= this->_n) 
+        throw std::length_error("ERROR: Span limit reached: cannot add more numbers");
     this->_container.push_back(new_n);
 }
 
+void Span::addMultipleNumbers(std::vector<int>::iterator begin, std::vector<int>::iterator end) {
+    if (std::distance(begin, end) + this->_container.size() > _n)
+        throw std::length_error("ERROR: Span limit reached: cannot add more numbers");
+    if (begin == end)
+        throw std::length_error("ERROR: No numbers to add.");
+    if (this->_container.empty())
+        this->_container.insert(this->_container.end(), begin, end);
+    else
+        this->_container.insert(this->_container.end(), begin, end);
+}
+
 int Span::shortestSpan(void){
-    // return the max value in the container, belongs to the lib algorithm
     if (this->_container.size() < 2)
-        throw std::length_error("No enough numbers in Span to get a short value.");
-    int max = *std::max_element(this->_container.begin(), this->_container.end());
-    return max;
+        throw std::length_error("ERROR: No enough numbers in Span to get a short value.");
+    std::vector <int> sorted = this->_container;
+    std::sort(sorted.begin(), sorted.end());
+    int dist = std::numeric_limits<int>::max();
+    for (size_t i = 0; i < sorted.size() - 1; i++){
+        int new_dist = sorted[i + 1] - sorted[i];
+        if (dist > new_dist)
+            dist = std::abs(new_dist);
+    }
+    return dist;
 }
 
 int Span::longestSpan(void){
     if (this->_container.size() < 2)
-            throw std::length_error("No enough numbers in Span to get a short value.");
-    int max = *std::max_element(this->_container.begin(), this->_container.end());
-    return max;
+        throw std::length_error("ERROR: No enough numbers in Span to get a long value.");
+    std::vector<int> sorted = this->_container;
+    std::sort(sorted.begin(), sorted.end());
+    return sorted.back() - sorted.front();
 }
 
 
